9e44b04ef529442f2f92f0df45d9434092a6615c
Kever Yang <kever.yang@rock-chips.com>
2016-09-01 10:14:21 +0800

    board: evb-rk3399: add api to support dwc3 gadget
    This patch add board_usb_init() and interrupt callback
    for dwc3 gadget.
    
    Signed-off-by: Kever Yang <kever.yang@rock-chips.com>

--- board/rockchip/evb_rk3399/evb-rk3399.c
+++ board/rockchip/evb_rk3399/evb-rk3399.c
@@ -9,6 +9,9 @@
 #include <dm/uclass-internal.h>
 #include <asm/arch/periph.h>
 #include <power/regulator.h>
+#include <asm/armv8/mmu.h>
+#include <usb.h>
+#include <dwc3-uboot.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -73,3 +76,52 @@ void dram_init_banksize(void)
 	gd->bd->bi_dram[0].start = 0x200000;
 	gd->bd->bi_dram[0].size = 0x80000000;
 }
+
+#ifdef CONFIG_USB_DWC3
+static struct dwc3_device dwc3_device_data = {
+	.index = 0,
+};
+
+int usb_gadget_handle_interrupts(void)
+{
+	dwc3_uboot_handle_interrupt(0);
+	return 0;
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	const void *blob = gd->fdt_blob;
+	int node;
+	const char *string;
+
+	node = fdt_node_offset_by_compatible(blob, -1,
+			"rockchip,rk3399-xhci");
+	if (node < 0) {
+		debug("%s dwc3 node not found\n", __func__);
+	} else {
+		dwc3_device_data.base = fdtdec_get_addr(blob, node, "reg");
+		dwc3_device_data.dis_u2_susphy_quirk =
+			fdtdec_get_int(blob, node,
+				       "snps,dis-u2-susphy-quirk", -1);
+
+		string = fdt_getprop(blob, node, "maximum-speed", NULL);
+		if (string) {
+			if (0 == strcmp(string, "super-speed"))
+				dwc3_device_data.maximum_speed = USB_SPEED_SUPER;
+			else if (0 == strcmp(string, "high-speed"))
+				dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
+			else if (0 == strcmp(string, "full-speed"))
+				dwc3_device_data.maximum_speed = USB_SPEED_FULL;
+			else
+				debug("%s: Cannot decode speed'%s'\n", __func__,
+				      string);
+		} else {
+			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
+		}
+		/* Hardcode controller in peripheral mode */
+		dwc3_device_data.dr_mode = USB_DR_MODE_PERIPHERAL;
+	}
+
+	return dwc3_uboot_init(&dwc3_device_data);
+}
+#endif
