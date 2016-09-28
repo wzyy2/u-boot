050d0f0ebf8a67c8d913bbda57d35d3f6d87621b
Jacob Chen <jacob2.chen@rock-chips.com>
2016-09-28 17:00:39 +0800

    rockchip: use rockchip linux partitions map
    Unify the partitions of each chip then it will be more easy for us to write scripts, tools or guides
     for rockchip chips.
    
    Those extra partitions mostly are used to be compatible with our internal loaders (such as miniloader
     which was same as spl,  or android loader then we can support dual boot)
    
    Change-Id: Ia2c41adafc6b7ea21d1711a052f00b88a3a927bc
    
    Conflicts:
    	include/configs/rk3036_common.h

--- include/configs/rk3036_common.h
+++ include/configs/rk3036_common.h
@@ -88,6 +88,17 @@
 	"kernel_addr_r=0x62000000\0" \
 	"ramdisk_addr_r=0x64000000\0"
 
+#define CONFIG_RANDOM_UUID
+#define PARTS_DEFAULT \
+	"uuid_disk=${uuid_gpt_disk};" \
+	"name=loader1,start=32K,size=4000K,uuid=${uuid_gpt_loader1};" \
+	"name=reserved1,size=64K,uuid=${uuid_gpt_reserved1};" \
+	"name=reserved2,size=4M,uuid=${uuid_gpt_reserved2};" \
+	"name=loader2,size=4MB,uuid=${uuid_gpt_loader2};" \
+	"name=atf,size=4M,uuid=${uuid_gpt_atf};" \
+	"name=boot,size=128M,bootable,uuid=${uuid_gpt_boot};" \
+	"name=rootfs,size=-,uuid=${uuid_gpt_rootfs};\0" \
+
 /* First try to boot from SD (index 0), then eMMC (index 1 */
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 0) \
--- include/configs/rk3399_common.h
+++ include/configs/rk3399_common.h
@@ -67,7 +67,12 @@
 #define CONFIG_PARTITION_UUIDS
 #define PARTS_DEFAULT \
 	"uuid_disk=${uuid_gpt_disk};" \
-	"name=boot,start=16M,size=32M,bootable;" \
+	"name=loader1,start=32K,size=4000K,uuid=${uuid_gpt_loader1};" \
+	"name=reserved1,size=64K,uuid=${uuid_gpt_reserved1};" \
+	"name=reserved2,size=4M,uuid=${uuid_gpt_reserved2};" \
+	"name=loader2,size=4MB,uuid=${uuid_gpt_loader2};" \
+	"name=atf,size=4M,uuid=${uuid_gpt_atf};" \
+	"name=boot,size=128M,bootable,uuid=${uuid_gpt_boot};" \
 	"name=rootfs,size=-,uuid=${uuid_gpt_rootfs};\0" \
 
 /* First try to boot from SD (index 0), then eMMC (index 1) */
